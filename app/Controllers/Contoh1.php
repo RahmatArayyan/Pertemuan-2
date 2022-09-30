5520dbfa4eac8cdb104ea45d2130dc3d03c3f2ac
rahmatarayyan <rahmatarayyan26@gmail.com>
2022-09-30 17:19:20 +0700

    menambahkan tugas latihan1

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rahmat Arayyan
+              Saya tinggal di daerah Jakarta Utara
+              olah raga yang saya sukai adalah
+              Bulutangkis dan Basket";
+    }
+}
